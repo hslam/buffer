df42ad9dbddd5c7313acea4d4de3a8011370e20f
hslam <791874158@qq.com>
2021-03-24 15:26:08 +0800

    add a unit option

--- buffer.go
+++ buffer.go
@@ -1,30 +1,35 @@
 // Copyright (c) 2021 Meng Huang (mhboy@outlook.com)
 // This package is licensed under a MIT license that can be found in the LICENSE file.
 
+// Package buffer implements a variable-sized bytes pool.
 package buffer
 
 import (
 	"sync"
 )
 
-const numShards = 256
+const (
+	numBuckets = 256
+	unit       = 1024
+)
 
-type buffers struct {
-	shards [numShards]Map
+type Buffers struct {
+	unit    int
+	buckets [numBuckets]bucket
 }
 
-type Map struct {
-	lock sync.RWMutex
-	data map[int]*Pool
+type bucket struct {
+	lock  sync.RWMutex
+	pools map[int]*Pool
 }
 
 type Pool struct {
-	pool *sync.Pool
 	size int
+	pool *sync.Pool
 }
 
 func (p *Pool) GetBuffer(size int) []byte {
-	return p.pool.Get().([]byte)
+	return p.pool.Get().([]byte)[:size]
 }
 
 func (p *Pool) PutBuffer(buf []byte) {
@@ -34,40 +39,59 @@ func (p *Pool) PutBuffer(buf []byte) {
 	}
 }
 
-func newBuffers() *buffers {
-	b := new(buffers)
-	for i := range b.shards {
-		b.shards[i].data = make(map[int]*Pool)
+func NewBuffers(unit int) *Buffers {
+	b := new(Buffers)
+	for i := range b.buckets {
+		b.buckets[i].pools = make(map[int]*Pool)
 	}
+	b.unit = unit
 	return b
 }
 
-func (b *buffers) AssignPool(size int) *Pool {
-	m := &b.shards[size%numShards]
+func (b *Buffers) AssignPool(size int) (p *Pool) {
+	if b.unit > 0 && size%b.unit > 0 {
+		size = size/b.unit*b.unit + b.unit
+	}
+	m := &b.buckets[size%numBuckets]
+	var ok bool
 	m.lock.RLock()
-	if p, ok := m.data[size]; ok {
+	if p, ok = m.pools[size]; ok {
 		m.lock.RUnlock()
-		return p
+		return
 	}
 	m.lock.RUnlock()
 	m.lock.Lock()
-	if p, ok := m.data[size]; ok {
-		m.lock.Unlock()
-		return p
+	if p, ok = m.pools[size]; !ok {
+		p = &Pool{
+			pool: &sync.Pool{New: func() interface{} {
+				return make([]byte, size)
+			}},
+			size: size,
+		}
+		m.pools[size] = p
 	}
-	p := &Pool{
-		pool: &sync.Pool{New: func() interface{} {
-			return make([]byte, size)
-		}},
-		size: size,
-	}
-	m.data[size] = p
 	m.lock.Unlock()
-	return p
+	return
 }
 
-var defaultBuffers = newBuffers()
+func (b *Buffers) GetBuffer(size int) []byte {
+	return b.AssignPool(size).GetBuffer(size)
+}
+
+func (b *Buffers) PutBuffer(buf []byte) {
+	b.AssignPool(cap(buf)).PutBuffer(buf)
+}
+
+var defaultBuffers = NewBuffers(unit)
 
 func AssignPool(size int) *Pool {
 	return defaultBuffers.AssignPool(size)
+}
+
+func GetBuffer(size int) []byte {
+	return defaultBuffers.GetBuffer(size)
+}
+
+func PutBuffer(buf []byte) {
+	defaultBuffers.PutBuffer(buf)
 }
--- buffer_test.go
+++ buffer_test.go
@@ -4,45 +4,167 @@
 package buffer
 
 import (
-	"sync"
 	"testing"
 )
 
+func TestUnit(t *testing.T) {
+	b := NewBuffers(0)
+	units := []int{0, 1, 512, 1023, 1024, 1024}
+	sizes := []int{1024, 1024, 1024, 1024, 1024, 1025}
+	results := []int{1024, 1024, 1024, 2046, 1024, 2048}
+	if len(units) != len(sizes) && len(sizes) != len(results) {
+		t.Error()
+	}
+	for i := 0; i < len(units); i++ {
+		b.unit = units[i]
+		size := sizes[i]
+		if b.unit > 0 && size%b.unit > 0 {
+			size = size/b.unit*b.unit + b.unit
+		}
+		if size != results[i] {
+			t.Error(i, size, results[i])
+		}
+	}
+}
+
 func TestAssignPool(t *testing.T) {
-	for i := 0; i < 2; i++ {
-		size := 64 * 1024
+	defaultBuffers = NewBuffers(1024)
+	for i := 0; i < 4; i++ {
+		size := 64*1024 + i
 		p := AssignPool(size)
-		buf := p.GetBuffer(size)
+		if p.size < size {
+			t.Error(p.size)
+		}
+		buf := GetBuffer(size)
 		if len(buf) < size {
 			t.Error(len(buf))
 		}
-		p.PutBuffer(buf)
-	}
-	wg := sync.WaitGroup{}
-	for i := 0; i < numShards*10; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for j := 0; j < 64*1024; j++ {
-				p := AssignPool(j)
-				if p.size != j {
-					t.Errorf("pool length error: %d != %d", p.size, j)
-				}
-			}
-		}()
+		PutBuffer(buf)
 	}
 }
 
 func BenchmarkAssignPool(b *testing.B) {
-	bs := newBuffers()
+	bs := NewBuffers(0)
 	for i := 0; i < b.N; i++ {
-		size := i % numShards * 1024
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit2(b *testing.B) {
+	bs := NewBuffers(2)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit4(b *testing.B) {
+	bs := NewBuffers(4)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit8(b *testing.B) {
+	bs := NewBuffers(8)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit16(b *testing.B) {
+	bs := NewBuffers(16)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit32(b *testing.B) {
+	bs := NewBuffers(32)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit64(b *testing.B) {
+	bs := NewBuffers(64)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit128(b *testing.B) {
+	bs := NewBuffers(128)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit256(b *testing.B) {
+	bs := NewBuffers(256)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit512(b *testing.B) {
+	bs := NewBuffers(512)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit1024(b *testing.B) {
+	bs := NewBuffers(1024)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit2048(b *testing.B) {
+	bs := NewBuffers(2048)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit4096(b *testing.B) {
+	bs := NewBuffers(4096)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit8192(b *testing.B) {
+	bs := NewBuffers(8192)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignPoolUnit16384(b *testing.B) {
+	bs := NewBuffers(16384)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
 		bs.AssignPool(size)
 	}
 }
 
 func BenchmarkAssignSizedPool(b *testing.B) {
-	bs := newBuffers()
+	bs := NewBuffers(0)
 	size := 64 * 1024
 	bs.AssignPool(size)
 	for i := 0; i < b.N; i++ {
@@ -51,18 +173,284 @@ func BenchmarkAssignSizedPool(b *testing.B) {
 }
 
 func BenchmarkBuffers(b *testing.B) {
-	bs := newBuffers()
+	bs := NewBuffers(0)
 	for i := 0; i < b.N; i++ {
-		size := i % numShards * 1024
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit2(b *testing.B) {
+	bs := NewBuffers(2)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit4(b *testing.B) {
+	bs := NewBuffers(4)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit8(b *testing.B) {
+	bs := NewBuffers(8)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit16(b *testing.B) {
+	bs := NewBuffers(16)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit32(b *testing.B) {
+	bs := NewBuffers(32)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit64(b *testing.B) {
+	bs := NewBuffers(64)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit128(b *testing.B) {
+	bs := NewBuffers(128)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit256(b *testing.B) {
+	bs := NewBuffers(256)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit512(b *testing.B) {
+	bs := NewBuffers(512)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit1024(b *testing.B) {
+	bs := NewBuffers(1024)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit2048(b *testing.B) {
+	bs := NewBuffers(2048)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit4096(b *testing.B) {
+	bs := NewBuffers(4096)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit8192(b *testing.B) {
+	bs := NewBuffers(8192)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkBuffersUnit16384(b *testing.B) {
+	bs := NewBuffers(16384)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
 		buf := bs.AssignPool(size).GetBuffer(size)
 		bs.AssignPool(size).PutBuffer(buf)
 	}
 }
 
 func BenchmarkAssignPoolAndBuffers(b *testing.B) {
-	bs := newBuffers()
+	bs := NewBuffers(0)
 	for i := 0; i < b.N; i++ {
-		size := i % numShards * 1024
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit2(b *testing.B) {
+	bs := NewBuffers(2)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit4(b *testing.B) {
+	bs := NewBuffers(4)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit8(b *testing.B) {
+	bs := NewBuffers(8)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit16(b *testing.B) {
+	bs := NewBuffers(16)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit32(b *testing.B) {
+	bs := NewBuffers(32)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit64(b *testing.B) {
+	bs := NewBuffers(64)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit128(b *testing.B) {
+	bs := NewBuffers(128)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit256(b *testing.B) {
+	bs := NewBuffers(256)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit512(b *testing.B) {
+	bs := NewBuffers(512)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit1024(b *testing.B) {
+	bs := NewBuffers(1024)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit2048(b *testing.B) {
+	bs := NewBuffers(2048)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit4096(b *testing.B) {
+	bs := NewBuffers(4096)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit8192(b *testing.B) {
+	bs := NewBuffers(8192)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffersUnit16384(b *testing.B) {
+	bs := NewBuffers(16384)
+	for i := 0; i < b.N; i++ {
+		size := i % (64 * 1024)
 		p := bs.AssignPool(size)
 		buf := p.GetBuffer(size)
 		p.PutBuffer(buf)
@@ -70,7 +458,7 @@ func BenchmarkAssignPoolAndBuffers(b *testing.B) {
 }
 
 func BenchmarkSizedBuffer(b *testing.B) {
-	bs := newBuffers()
+	bs := NewBuffers(0)
 	size := 64 * 1024
 	p := bs.AssignPool(size)
 	for i := 0; i < b.N; i++ {
