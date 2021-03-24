969b58a891331e2655ac663ad51db9eaf744c7eb
hslam <791874158@qq.com>
2021-03-24 11:25:23 +0800

    add benchmarks

--- buffer_test.go
+++ buffer_test.go
@@ -18,3 +18,49 @@ func TestAssignPool(t *testing.T) {
 		p.PutBuffer(buf)
 	}
 }
+
+func BenchmarkAssignPool(b *testing.B) {
+	bs := newBuffers()
+	for i := 0; i < b.N; i++ {
+		size := i % numShards * 1024
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkAssignSizedPool(b *testing.B) {
+	bs := newBuffers()
+	size := 64 * 1024
+	bs.AssignPool(size)
+	for i := 0; i < b.N; i++ {
+		bs.AssignPool(size)
+	}
+}
+
+func BenchmarkBuffers(b *testing.B) {
+	bs := newBuffers()
+	for i := 0; i < b.N; i++ {
+		size := i % numShards * 1024
+		buf := bs.AssignPool(size).GetBuffer(size)
+		bs.AssignPool(size).PutBuffer(buf)
+	}
+}
+
+func BenchmarkAssignPoolAndBuffers(b *testing.B) {
+	bs := newBuffers()
+	for i := 0; i < b.N; i++ {
+		size := i % numShards * 1024
+		p := bs.AssignPool(size)
+		buf := p.GetBuffer(size)
+		p.PutBuffer(buf)
+	}
+}
+
+func BenchmarkSizedBuffer(b *testing.B) {
+	bs := newBuffers()
+	length := 64 * 1024
+	p := bs.AssignPool(length)
+	for i := 0; i < b.N; i++ {
+		buf := p.GetBuffer(length)
+		p.PutBuffer(buf)
+	}
+}
